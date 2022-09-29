13ccd8455c003a43db599f7cab5e76f2ae2a51a3
solmoyano <solmoyano1012@gmail.com>
2022-09-29 08:09:23 -0300

    version 0.1.9 front

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,54 +1,64 @@
-import React from 'react'
-import '../styles/Home.css'
-import perfilDefault from '../multimedia/profile-picture.png'
+import React from 'react';
+import '../styles/Home.css';
+import perfilDefault from '../multimedia/profile-picture.png';
 
 export default function Home() {
 
   const jsonHome={
     servicesArray:[
       {
-        category:'Manicura y pedicura',
+        category:'Manicura y Pedicura',
+        photo:'',
         listServices:[
           'Semipermanente',
           'Kapping',
-          'Uñas acrilicas'
+          'Uñas acrilicas',
+          'Esculpido'
         ]
       },
       {
-        category:'Tratamientos faciales',
+        category:'Tratamientos Faciales',
+        photo:'',
         listServices:[
           'Hiperpigmentación',
           'Dermoabración',
           'Peeling',
-          'Luz pulsada'
+          'Luz pulsada',
+          'Exfoliación'
         ]
       },
       {
-        category:'Tratamientos corporales',
+        category:'Tratamientos Corporales',
+        photo:'',
         listServices:[
-          'Hiperpigmentación',
-          'Dermoabración',
-          'Peeling',
-          'Luz pulsada'
+          'Tratamientos reafirmantes',,
+          'Termoterapia',
+          'Presoterapia',
+          'Zionic',
+          'Exfoliación'
         ]
       },
       {
-        category:'Cejas y pestañas',
+        category:'Depilación Definitiva',
+        photo: '',
         listServices:[
-          'Perfilado',
-          'Lifting',
-          'Microblanding',
-          'Microshading'
+          'Axilas',
+          'Bozo',
+          'Dedos',
+          'Espalda',
+          'Pecho',
+          'Semi Cavado',
+          'Cavado Completo',
+          'Media Pierna',
+          'Piernas completas',
+          'Dos zonas',
+          'Tres Zonas',
+          'Cuatro zonas'
         ]
       },
       {
-        category:'Depilación definitiva',
-        listServices:[
-          'Cuerpo completo'
-        ]
-      },
-      {
-        category:'Cejas y pestañas',
+        category:'Cejas y Pestañas',
+        photo: '',
         listServices:[
           'Perfilado',
           'Lifting',
@@ -57,44 +67,62 @@ export default function Home() {
         ]
       }
     ],
+
     professionalsArray:[
       {
-        name:'Nombre 1',
-        lastname:'Apellido 1',
+        name:'Noelia',
+        lastname:'Barrionuevo',
         photo:'https://scontent.fsla4-1.fna.fbcdn.net/v/t1.18169-9/10401972_472787482918821_4958068379902033714_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=apCnGm0dF4QAX-RePPq&_nc_ht=scontent.fsla4-1.fna&oh=00_AT_TlF4FmZT2JtkWv41ftgvbRJ2B6GlXXkb3-abnz-Dt2Q&oe=63570F7A'
       },{
-        name:'Nombre 2',
-        lastname:'Apellido 2',
+        name:'Sofía',
+        lastname:'Malestrini',
+        photo:''
+      },{
+        name:'Marcela',
+        lastname:'Oliva',
+        photo:''
+      },{
+        name:'Julieta',
+        lastname:'Vega',
         photo:''
       }
     ]
   }
 
   return (
-    <div>
-        <div>
-            <div className='flexRow'>
-                <h1>Nuestros servicios</h1>
-            </div>
+    <div className='divContainerHome'>
+        
+        <h1 className='homeTitle'>Nuestros servicios</h1>
 
-            <div  className='categories'>
+        <div  className='categories'>
+            
+            {jsonHome.servicesArray.map( (eachService,index) =>{
                 
-                {jsonHome.servicesArray.map( (eachService,index) =>
-                    <div key={index} className='category'>
-                        <div>
-                            <h3>
-                            {eachService.category}
-                            </h3>
-                        </div>
+                return(
 
-                        {eachService.listServices.map( (service,index2)=>
-                            <div className='servicio' key={index2}>
-                            {service}
+                    <div key={index} className='category'>
+                    
+                        <h3> {eachService.category} </h3>
+
+                        <div>  
+                            <img 
+                            src={require(`../multimedia/cejas_y_pestanias.jpg`)} 
+                            alt='Service Image'
+                            className='serviceImg'
+                            />  
+                            <div className='divListServices'>
+                                {eachService.listServices.map( (service,index2)=>{
+                                    return(
+                                        <ul className='servicesList' key={index2}>
+                                            <li className='eachServiceList'>{service}</li>
+                                        </ul>
+                                    )
+                                })}
                             </div>
-                        )}
+                        </div>
                     </div>
-                )}
-            </div>
+                )
+            })}
         </div>
 
         <div>
@@ -103,16 +131,14 @@ export default function Home() {
             </div>
             
             <div className='flexRow'>
-
                 {jsonHome.professionalsArray.map( (eachProfessional,index) =>
                     <div className='profCard flexColumn' key={index}>
-                        <div className='fotoPerfil'>
+                        <div className='profilePhoto'>
                             <img src={ eachProfessional.photo? eachProfessional.photo : perfilDefault } alt=""/> 
                         </div>
                         <p>{eachProfessional.lastname+' '+eachProfessional.name}</p>
                     </div>
                 )}
-            
             </div>
         </div>
     </div>
---
+++ frontend/src/components/ServiceDetails.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+function ServiceDetails (){
+    
+    useEffect( () =>{
+        const urlAPI= ''
+
+    })
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default ServiceDetails;
