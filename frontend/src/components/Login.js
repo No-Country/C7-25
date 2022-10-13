c571c4bd3e3c1584151c9369edeff3f849761fa7
Rocabado Cristian <cmrocabado@gmail.com>
2022-10-13 10:57:46 -0300

    version 0.3.3 front

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,47 +1,8 @@
-import { useNavigate } from 'react-router-dom';
-import '../styles/LogIn.css';
-import {logIn} from '../services/API'
-
-
-function LogIn() {
-
-  let handleLogin = (e) =>{
-    e.preventDefault();
-
-    const inputEmail= e.target.userEmail.value;
-    const inputPassword = e.target.userPassword.value;
-
-    const bodyAPI = new URLSearchParams();
-    bodyAPI.append('email', inputEmail);
-    bodyAPI.append('password', inputPassword);
-
-    logIn(bodyAPI); //Ejecuto la API
-  }
-  
-  let navigate = useNavigate();
-
-  let goToSignUp = ()=>{
-    navigate('/signup');
-  }
-
-  return (
-    <div className='divContainerLogIn'>
-        <h1 className='LogInTitle'>Inicia Sesión</h1>
-        <form onSubmit={handleLogin} className='logInForm'>
-            <label>Correo Electrónico</label><br/>
-            <input type='email' name='userEmail' required='required'/><br/>
-
-            <label>Contraseña</label><br/>
-            <input type='password' name='userPassword' required='required'/><br/>
-
-            <div className='divBtnLogIn'>
-              <button  type='submit' className='btnLogIn'>Iniciar Sesión</button>
-              <button onClick={goToSignUp} className='btnGoToSignUp'>Registrarse</button>
-            </div>
-
-        </form>
-    </div>
-  )
+function LogOut(){
+    return(
+        <div>
+            Cerrar Sesión
+        </div>
+    )
 }
-
-export default LogIn;
+export default LogOut;
