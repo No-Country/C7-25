1203c5b2f72ea9ccc516d9efa7c0182c45604ce2
solmoyano <solmoyano1012@gmail.com>
2022-10-19 10:06:47 -0300

    version 0.3.19 front

--- frontend/src/components/FormHome.js
+++ frontend/src/components/FormHome.js
@@ -44,16 +44,16 @@ function FormHome(){
             <h1 className='FormsTitle'>Editar</h1>
             <form onSubmit={handleEditHome} className='formForms'>
                 <label>Nombre empresa:</label><br/>
-                <input type='text' name='brandNameInput'/>
+                <input type='text' name='brandNameInput' value={brandName} onChange={(e) => setBrandName(e.target.value)}/><br/>
 
                 <label>Direción:</label><br/>
-                <input type='text' name='adressInput'/><br/>
+                <input type='text' name='adressInput' value={adress} onChange={(e) => setAdress(e.target.value)}/><br/>
 
                 <label>Número de contacto:</label><br/>
-                <input type='tel' name='telephoneInput'/><br/>
+                <input type='tel' name='telephoneInput' value={telephone} onChange={(e) => setTelephone(e.target.value)}/><br/>
                 
                 <label>Descripción de la empresa:</label><br/>
-                <input type='text' name='descriptionInput'/><br/>
+                <input type='text' name='descriptionInput' value={description} onChange={(e) => setDescription(e.target.value)}/><br/>
 
                 <div className='divEditBtn'>
                     <button type='submit'>Editar</button>
